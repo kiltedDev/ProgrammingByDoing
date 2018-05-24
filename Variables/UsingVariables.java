c9ab0c751f7d03f07381d99eec82425c517638e8
Thomas Wilson <twilson.spqr@gmail.com>
2018-05-24 13:51:39 -0400

    using variables

---
+++ Variables/UsingVariables.java
@@ -0,0 +1,11 @@
+public class UsingVariables {
+  public static void main( String[] args) {
+    int i = 113;
+    String task = "Computer Science";
+    double dub = 2.71828;
+
+    System.out.println( "This is room # " + i + "." );
+    System.out.println("e is close to " + dub + "." );
+    System.out.println("I'm learning a bit about " + task + "." );
+  }
+}
