4c43359c0b6a4c89ef1979a6cfad8bc2543aea91
Thomas Wilson <twilson.spqr@gmail.com>
2018-05-24 13:56:55 -0400

    Yet more variables

---
+++ Variables/StillUsingVariables.java
@@ -0,0 +1,8 @@
+public class StillUsingVariables {
+  public static void main( String[] args) {
+    int year = 2017;
+    String name = "Thomas Wilson";
+
+    System.out.println( "My name is " + name + " and I will graduate in " + year + "." );
+  }
+}
