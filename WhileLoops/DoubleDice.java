ac4ea40cfa051fb86af6793a9b7921c22b215202
Thomas Wilson <twilson.spqr@gmail.com>
2018-06-11 19:23:34 -0400

    learning doWhile

--- WhileLoops/DoubleDice.java
+++ WhileLoops/DoubleDice.java
@@ -3,11 +3,11 @@ import java.util.Random;
 public class DoubleDice {
   public static void main( String[] args) {
     Random r = new Random();
-    int roll1=0, roll2=1, sum;
+    int roll1, roll2, sum;
 
     System.out.println("Here come the dice!");
-    
-    while (roll1 != roll2) {
+
+    do {
       roll1 = 1 + r.nextInt(6);
       roll2 = 1 + r.nextInt(6);
       sum = roll1 + roll2;
@@ -15,6 +15,6 @@ public class DoubleDice {
       System.out.println("\nRoll 1: " + roll1);
       System.out.println("Roll 2: " + roll2);
       System.out.println("\nTotal: " + sum);
-    }
+    } while (roll1 != roll2);
   }
 }
--- WhileLoops/KeepGuessing.java
+++ WhileLoops/KeepGuessing.java
@@ -5,17 +5,19 @@ public class KeepGuessing {
   public static void main(String[] args) {
     Random r = new Random();
     Scanner keyboard = new Scanner(System.in);
-    int guess, answer = 1 + r.nextInt(10), tries = 1;
+    int guess, answer = 1 + r.nextInt(10), tries = 0;
 
     System.out.println("HA!  I AM THINKING OF NUMBER.  YOU CANNOT GUESS NUMBER,,,");
     System.out.print("GO AHEAD AND TRY. 1-10.  DO IT NERD.\n> ");
-    guess = keyboard.nextInt();
+    // answer = 1; //debugging
 
-    while (guess != answer) {
-    System.out.print("MWA HA HA!  FOOL YOU WERE WRONG!  I WAS RIGHT!  I BET YOU STILL CAN'T GUESS IT!\n>");
-    guess = keyboard.nextInt();
-    tries++;
-    }
+    do {
+      guess = keyboard.nextInt();
+      if (guess != answer) {
+        System.out.print("MWA HA HA!  FOOL YOU WERE WRONG!  I WAS RIGHT!  I BET YOU STILL CAN'T GUESS IT!\n> ");
+      }
+      tries++;
+    } while (guess != answer);
 
     System.out.println("OH NOES!!!1! YOU DID IT! IT WAS TOTALLY " + answer);
     System.out.println("BUT!,! IT TOOK YOU, LIKE, " + tries + " TIMES TO GET IT!  I STILL WIN!");
