743675bebd27133f65135ad78e97bd560d1fd652
Thomas Wilson <twilson.spqr@gmail.com>
2018-06-11 20:18:43 -0400

    DRYed up the triangle thing

--- WhileLoops/RightTriangleChecker.java
+++ WhileLoops/RightTriangleChecker.java
@@ -2,34 +2,13 @@ import java.util.Scanner;
 
 public class RightTriangleChecker {
   public static void main(String[] args) {
-    Scanner keyboard = new Scanner(System.in);
     int a, b, c;
 
     System.out.println("Enter three positive integers in ascending order:");
-    do {
-      System.out.print("Side 1: ");
-      a = keyboard.nextInt();
-      if (a < 1)
-        System.out.println("Positive intergers only please.");
-    } while (a < 1);
 
-    do {
-      System.out.print("Side 2: ");
-      b = keyboard.nextInt();
-      if (b < 1)
-        System.out.println("Positive intergers only please. Try again.");
-      else if (b < a)
-        System.out.println(b + " is smaller than " + a + ".  Try again.");
-    } while (b < 1 || b < a);
-
-    do {
-      System.out.print("Side 3: ");
-      c = keyboard.nextInt();
-      if (c < 1)
-        System.out.println("Positive intergers only please. Try again.");
-      else if (c < b)
-        System.out.println(c + " is smaller than " + b + ".  Try again.");
-    } while (c < 1 || c < b);
+    a = takeSides(0,1);
+    b = takeSides(a,2);
+    c = takeSides(b,3);
 
     System.out.println("Your three sides are " + a + " " + b + " " + c + ".");
     boolean righty = (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2));
@@ -38,5 +17,21 @@ public class RightTriangleChecker {
     } else {
       System.out.println("Sorry, this is not a right triangle.");
     }
+  }
+
+  public static int takeSides(int oldSide,  int workingSide) {
+    int newSide;
+    Scanner keyboard = new Scanner(System.in);
+
+    do {
+      System.out.print("Side "+ workingSide +": ");
+      newSide = keyboard.nextInt();
+      if (newSide < 1)
+        System.out.println("Positive intergers only please. Try again.");
+      else if (newSide < oldSide)
+        System.out.println(newSide + " is smaller than " + oldSide + ".  Try again.");
+    } while (newSide < 1 || newSide < oldSide);
+
+    return newSide;
   }
 }
