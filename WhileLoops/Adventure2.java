5c4e4efba7065d37612b798b370a53227aa3278c
Thomas Wilson <twilson.spqr@gmail.com>
2018-06-15 19:18:45 -0400

    simple CYOA, not exciting

--- WhileLoops/Adventure2.java
+++ WhileLoops/Adventure2.java
@@ -2,75 +2,110 @@ import java.util.Scanner;
 
 public class Adventure2 {
   public static void main(String[] args) {
-    Scanner kb = new Scanner(System.in);
-    String location = "streets";
-    boolean girl = false, caseClosed = false;
+    Scanner keyboard = new Scanner(System.in);
+    String location = "start";
+    boolean  inactive = false;
 
-    while (!caseClosed) {
-      if (location == "streets") {
-        System.out.println("You're Philly's only PI Sorcerer, on a case to find a missing child.");
-        System.out.println("You've been using a tracking spell and have narrowed your search to Penn's Landing.");
-        System.out.println("Your options are to check out the PARK or the sailing BOAT.");
-        System.out.print("Remember, though, you're a sorcerer.  You can always cast a SPELL.\n> ");
-        String action = kb.nextLine().toLowerCase();
-        if (action.equals("spell") ) {
-          System.out.println(spell(location) );
-        } else if (action.equals("boat") ) {
-          location = "boat"
-        } else if (action.equals("park") ) {
-          location = "park"
+    while (!inactive) {
+      if (location == "start") {
+        System.out.println("Description of Start.");
+        System.out.println("Description of route to Option1.");
+        System.out.println("Description of route to Option2.");
+        System.out.print("> ");
+        String action = keyboard.nextLine().toLowerCase();
+        if (action.equals("option1") ) {
+          location = "option1";
+        } else if (action.equals("option2") ) {
+          location = "option2";
         }
       }
 
-      if (location == "boat") {
-        System.out.println("You're on the Spirit of Philadelphia.");
-        System.out.println("This boat has a lot of history, but you can read about that later.");
-        System.out.println("You don't see any sign of the girl, you can head back to the STREETS or try BELOW./n");
-        String action = kb.nextLine().toLowerCase();
-        if (action.equals("spell") ) {
-          System.out.println(spell(location) );
-        } else if (action.equals("streets") ) {
-          location = "streets"
-        } else if (action.equals("below") ) {
-          location = "below"
+      if (location == "option1") {
+        System.out.println("Description of Option1.");
+        System.out.println("Description of route to Option11.");
+        System.out.println("Description of route to Option12.");
+        System.out.println("Description of route to Start.");
+        System.out.print("> ");
+        String action = keyboard.nextLine().toLowerCase();
+        if (action.equals("option11") ) {
+          location = "option11";
+        } else if (action.equals("option12") ) {
+          location = "option12";
+        } else if (action.equals("start") ) {
+          location = "start";
         }
       }
 
+      if (location == "option2") {
+        System.out.println("Description of Option2.");
+        System.out.println("Description of route to Option21.");
+        System.out.println("Description of route to Option22.");
+        System.out.println("Description of route to Start.");
+        System.out.print("> ");
+        String action = keyboard.nextLine().toLowerCase();
+        if (action.equals("option21") ) {
+          location = "option21";
+        } else if (action.equals("option22") ) {
+          location = "option22";
+        } else if (action.equals("start") ) {
+          location = "start";
+        }
+      }
 
+      if (location == "option11") {
+        System.out.println("Description of Option11.");
+        System.out.println("Description of route to Option1.");
+        System.out.println("Description of route to inactive.");
+        System.out.print("> ");
+        String action = keyboard.nextLine().toLowerCase();
+        if (action.equals("option1") ) {
+          location = "option1";
+        } else if (action.equals("inactive") ) {
+          inactive = true;
+        }
+      }
+
+      if (location == "option12") {
+        System.out.println("Description of Option12.");
+        System.out.println("Description of route to Option1.");
+        System.out.println("Description of route to inactive.");
+        System.out.print("> ");
+        String action = keyboard.nextLine().toLowerCase();
+        if (action.equals("option1") ) {
+          location = "option1";
+        } else if (action.equals("inactive") ) {
+          inactive = true;
+        }
+      }
+
+      if (location == "option21") {
+        System.out.println("Description of Option21.");
+        System.out.println("Description of route to Option2.");
+        System.out.println("Description of route to inactive.");
+        System.out.print("> ");
+        String action = keyboard.nextLine().toLowerCase();
+        if (action.equals("option2") ) {
+          location = "option2";
+        } else if (action.equals("inactive") ) {
+          inactive = true;
+        }
+      }
+
+      if (location == "option22") {
+        System.out.println("Description of Option22.");
+        System.out.println("Description of route to Option2.");
+        System.out.println("Description of route to inactive.");
+        System.out.print("> ");
+        String action = keyboard.nextLine().toLowerCase();
+        if (action.equals("option2") ) {
+          location = "option2";
+        } else if (action.equals("inactive") ) {
+          inactive = true;
+        }
+      }
 
       System.out.println("");
-      caseClosed = true;
     }
-  }
-
-  public static String spell(String location) {
-    String spellResult = "";
-    if (location.equals("streets") ) {
-      spellResult = "You try your Tracking Spell again. \nIt points towards the Spirit of Philadelphia, the BOAT.";
-    }
-    return spellResult;
+    System.out.println("You Win!");
   }
 }
-
-// Start on streets
-// directions school & bridge & car
-// SPELL: Location - girl towards bridge
-//if girl, car = win
-
-// school
-// SPELL finds locker, grafittied
-
-// bridge
-// directions back to streets or warehouse
-// if girl, then troll
-// if not, then search of docks turns
-
-// warehouse
-// directions leave
-// girl can be heard crying
-// SPELL Light, show girl
-
-// search
-// directions talk to girl
-// if light, talking
-// if not, screams and runs away.  Lost trail
